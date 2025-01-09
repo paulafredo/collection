efcc643c50706045cad884044fd9a88353f117a7
Mazino <fredozv01@gmail.com>
2025-01-09 23:13:43 +0100

    Tuples

---
+++ Tuples/main.py
@@ -0,0 +1,19 @@
+# COLLECTION 
+# Tuple  ():  immutable (non modifiable )
+# liste [] : mutable -> modifiable : rajout / suprimer des élement ou modifier 
+
+
+a = 5 
+b = "Toto"
+
+personnes = ("Melanie ", "Jean ", "Martin ", "Alice "  ) 
+# print(personnes)
+# print(len(personnes))
+# for i in range(0 , len(personnes)):
+#     print(personnes[i])
+   
+
+for i in personnes:
+    print(i)
+    print(len(i))
+    print(i [-1])
