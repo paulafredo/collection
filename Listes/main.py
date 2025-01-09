623753dc42d709131d2f8138a9be5b0379baca88
Mazino <fredozv01@gmail.com>
2025-01-09 23:26:58 +0100

    slice

---
+++ Listes/main.py
@@ -0,0 +1,24 @@
+# COLLECTION 
+# Tuple  ():  immutable (non modifiable )
+# liste [] : mutable -> modifiable : rajout / suprimer des élement ou modifier 
+
+
+a = 5 
+b = "Toto"
+
+personnes = ["Melanie ", "Jean ", "Martin ", "Alice "  ]
+# print(personnes)
+# print(len(personnes))
+# for i in range(0 , len(personnes)):
+#     print(personnes[i])
+
+
+# rajouter
+personnes.append("Paul")
+print(personnes)
+
+
+#  suprimme par index 
+# personnes.pop(1)
+# print(personnes)
+
--- Tuples/main.py
+++ Tuples/main.py
@@ -3,17 +3,43 @@
 # liste [] : mutable -> modifiable : rajout / suprimer des élement ou modifier 
 
 
-a = 5 
-b = "Toto"
+# a = 5 
+# b = "Toto"
 
-personnes = ("Melanie ", "Jean ", "Martin ", "Alice "  ) 
+# personnes = ("Melanie ", "Jean ", "Martin ", "Alice "  ) 
 # print(personnes)
 # print(len(personnes))
 # for i in range(0 , len(personnes)):
 #     print(personnes[i])
    
 
-for i in personnes:
+# for i in personnes:
+#     print(i)
+#     print(len(i))
+#     print(i [-1])
+
+
+# function et tuples 
+
+def obtenir_info_personne():
+    return "Melanie", 37, 1.64 
+
+# infos = obtenir_info_personne()
+# print(f"nom :  {infos[0]}")
+# print(f"age :  {infos[1]}")
+# print(f"taille :  {infos[2]}")
+
+
+# nom, age , taille = obtenir_info_personne()
+
+# info = (nom , age , taille )
+# print (info)
+
+
+# ''''''''SLICE (De :  a ) '''
+personnes = ("Melanie ", "Jean ", "Martin ", "Alice "  ) 
+
+for i in personnes[1: 3 ]: 
     print(i)
-    print(len(i))
-    print(i [-1])
+
+
